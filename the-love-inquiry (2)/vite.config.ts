7605eda77679665c1bebd799e9031bd70ed802e5
Shivangi Singh <63343409+shivangis2050@users.noreply.github.com>
2026-02-18 17:44:40 +0530

    Update vite.config.ts

--- the-love-inquiry (2)/vite.config.ts
+++ the-love-inquiry (2)/vite.config.ts
@@ -9,6 +9,8 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
+         base: "the-love-inquiry (2)",
+ 
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
